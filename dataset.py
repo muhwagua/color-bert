ebbe2fdf2cd1aadc710818f392cc6abf427c12b8
Jake Tae <jaesungtae@gmail.com>
2021-02-28 03:05:13 +0900

    chore: add skeleton files for project structure

---
+++ dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import DataLoader, Dataset
+
+
+class ColorDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
---
+++ train.py
@@ -0,0 +1,12 @@
+import argparse
+
+import torch
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
