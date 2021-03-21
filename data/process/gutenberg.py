efd02813b7e04706afb8b09a8a688dcee41810ed
Jake Tae <jaesungtae@gmail.com>
2021-03-21 15:34:39 +0900

    feature: complete gutenberg crawler script

--- data/process/gutenberg.py
+++ data/process/gutenberg.py
@@ -35,30 +35,46 @@ def has_color(sentence):
 def get_color_sentences(url):
     color_sentences = []
     response = requests.get(url)
-    soup = BeautifulSoup(response.content)
+    soup = BeautifulSoup(response.content, features="html.parser")
     p_tags = soup.find_all("p")
     for tag in p_tags:
-        chunk = tag.text.strip().replace("\r\n", " ")
-        sentences = chunk.split(".")
+        chunk = tag.text.replace("\r\n", " ")
+        chunk = re.sub("[\[\d\]]", "", chunk)
+        sentences = re.split("(?<=[.!?]) +", chunk)
         for sentence in sentences:
             if has_color(sentence):
                 sentence = " ".join(sentence.split())
-                color_sentences.append(sentence + ".")
+                color_sentences.append(sentence)
     return color_sentences
 
 
 def get_book_urls():
-    pass
+    book_urls = []
+    root = "https://www.gutenberg.org"
+    popular_books_url = "https://www.gutenberg.org/browse/scores/top#books-last1"
+    response = requests.get(popular_books_url)
+    soup = BeautifulSoup(response.content, features="html.parser")
+    book_list = soup.find("ol")
+    urls = [f"{root}{a['href']}" for a in book_list.find_all("a")]
+    for url in urls:
+        book_response = requests.get(url)
+        book_soup = BeautifulSoup(book_response.content, features="html.parser")
+        try:
+            a_tag = book_soup.find("a", text="Read this book online: HTML")
+            book_urls.append(f"{root}{a_tag['href']}")
+        except TypeError:
+            continue
+    return book_urls
 
 
 def main():
     all_sentences = []
-    urls = get_book_urls
+    urls = get_book_urls()
     for url in urls:
         color_sentences = get_color_sentences(url)
         all_sentences.extend(color_sentences)
     df = pd.DataFrame(all_sentences)
-    pd.save_csv(os.path.join(os.path.dirname(__file__), "raw", "gutenberg.csv"))
+    df.to_csv(os.path.join(os.path.dirname(__file__), "raw", "gutenberg.csv"))
 
 
 if __name__ == "__main__":
