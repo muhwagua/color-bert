b752971320752d14859e4b5c360ad09b4bfda689
ampehta <68820979+ampehta@users.noreply.github.com>
2021-03-24 14:08:53 +0900

    feature: fixed iteration

--- Model/network.py
+++ Model/network.py
@@ -5,7 +5,7 @@ from configuration import args
 TPU, Strategy, GlobalBatchSize = connect_TPU(args.batch_size_TPU)
 
 # X : train_text, y : train_label
-dist_dataset = create_distributed_dataset(Strategy,X,y,GlobalBatchSize,training=True)
+dist_dataset = create_distributed_dataset(Strategy,X,y,GlobalBatchSize)
 
 Model , Optimizer = get_model_and_optimizer(Strategy,args.vanilla_bert,args.learning_rate)
 train = TrainTpu(dist_dataset,Model,Optimizer,args,TPU,Strategy,GlobalBatchSize) # train_dist_dataset : dataset, model: bert-based-uncased
