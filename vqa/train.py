8341ef085b80e7bc4e4a117ee88cfbdee6f85b1e
Jake Tae <jaesungtae@gmail.com>
2021-04-16 01:14:31 +0900

    Revert "Revert "chore: rm raw file, add pl train notebooks""
    This reverts commit d2ecd1baed82798f5abb71a2425e85a50283208d.

---
+++ vqa/train.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--init_from", type=str, help="")
+    main()
